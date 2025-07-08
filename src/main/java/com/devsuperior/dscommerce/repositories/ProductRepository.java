c2e04251e1122f979ea70a435e2585a726b46402
José Froes <josepaulo.froes@gmail.com>
2025-07-08 15:49:44 -0300

    Get Product by id

--- src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.devsuperior.dscommerce.repositories;
 
-public interface ProductRepository {
+import com.devsuperior.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
